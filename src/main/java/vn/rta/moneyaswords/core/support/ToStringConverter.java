9533a22c2a7d901f309821c4bc461dd2a7ce0ea9
chunghoang <chunghoang@rta.vn>
2018-03-02 11:58:40 +0700

    Support jdk 7

--- src/main/java/vn/rta/moneyaswords/core/support/ToStringConverter.java
+++ src/main/java/vn/rta/moneyaswords/core/support/ToStringConverter.java
@@ -2,9 +2,15 @@ package vn.rta.moneyaswords.core.support;
 
 import vn.rta.moneyaswords.core.GenderAwareIntegerToWordsMapper;
 import vn.rta.moneyaswords.core.NumberToWordsConverter;
+import vn.rta.moneyaswords.core.languages.GenderType;
 
 public class ToStringConverter {
     public static GenderAwareIntegerToWordsMapper toGenderAwareInteger(final NumberToWordsConverter<Integer> integerToStringConverter) {
-        return (value, genderType) -> integerToStringConverter.asWords(value);
+        return new GenderAwareIntegerToWordsMapper() {
+            @Override
+            public String asWords(Integer value, GenderType genderType) {
+                return integerToStringConverter.asWords(value);
+            }
+        };
     }
 }
